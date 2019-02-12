fffcf0fb850bd5f6c3dd5a3464e0f6adab173628
Shubham Pednekar <32964043+txtr@users.noreply.github.com>
2019-02-13 00:53:59 +0530

    FCFS fully working
    Signed-off-by: Shubham Pednekar <32964043+txtr@users.noreply.github.com>

--- scheduling/fcfs.py
+++ scheduling/fcfs.py
@@ -1,38 +1,50 @@
+from functools import reduce
+
+
 class Process:
     def __init__(self, ID, AT, BT):
-        self.id      = ID
+        self.id = ID
         self.arrival = AT
-        self.burst   = BT
+        self.burst = BT
         self.execute = 0
-        self.close   = None
-    
-    def process(self, T):
+        self.close = None
+
+    def process(self, t):
         if self.execute < self.burst:
-            self.execute = self.execute+1
+            self.execute = self.execute + 1
             return True
         elif self.execute == self.burst:
-            self.close = T
+            self.close = t
             return False
 
 
-def sort(POOL):
-    return sorted(POOL, key=lambda x:x.arrival, reverse=False)
+def read():
+    processes = set()
+    n = int(input("Enter total no. of processes: "))
+    print("SYNTAX: <AT> <BT>")
+    for process_id in range(n):
+        print(process_id, ":", end=" ")
+        at, bt = map(int, input().split(" "))
+        processes.add(Process(process_id, at, bt))
+    return processes
 
 
-def simulate(POOL):
+def simulate(processes):
     time = 0
-    POOL = sort(POOL)
-    for proc in POOL:
-        while(proc.process(time)):
-            time = time+1
+    completed = list()
+    while processes:
+        next_process = reduce(lambda x, y: x if x.arrival < y.arrival else y, processes)
+        if next_process.arrival > time:
+            time += 1
+        elif next_process.process(time):
+            time += 1
+        else:
+            processes.remove(next_process)
+            completed.append(next_process)
+    for process in completed:
+        print(process.id, process.close, sep=" ")
 
 
 if __name__ == '__main__':
-    time = 0
-    t_proc = int(input("Total number of process : "))
-    pool = list[]
-    for proc_id in range(t_proc):
-        at, bt = input().split(" ")
-        proc = Process(ID, int(at), int(bt))
-        pool.append(proc)
-    simuate(pool)
+    simulate(read())
+    exit()
